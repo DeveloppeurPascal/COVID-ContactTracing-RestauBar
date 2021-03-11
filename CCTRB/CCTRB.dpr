1a8adba8cd0a92dde1afb49c58a5a3e8def1929d
patrick.premartin@developpeur-pascal.fr <Patrick>
2021-03-11 17:50:34 +0100

    creation du programme

---
+++ CCTRB/CCTRB.dpr
@@ -0,0 +1,14 @@
+program CCTRB;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  fPrincipale in 'fPrincipale.pas' {frmPrincipale};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TfrmPrincipale, frmPrincipale);
+  Application.Run;
+end.
