34271628f33fdfb24a066d1b1adbaf263881a2cd
patrick.premartin@developpeur-pascal.fr <Patrick>
2021-03-25 17:53:05 +0100

    gestion de la configuration du logiciel

---
+++ EtabCCTRB/uConfig.pas
@@ -0,0 +1,63 @@
+unit uConfig;
+
+interface
+
+type
+  TConfig = class
+  private
+    class function getID: integer; static;
+    class function getIDTypeEtablissement: integer; static;
+    class function getRaisonSociale: string; static;
+    class procedure setID(const Value: integer); static;
+    class procedure setIDTypeEtablissement(const Value: integer); static;
+    class procedure setRaisonSociale(const Value: string); static;
+  public
+    class property id: integer read getID write setID;
+    class property RaisonSociale: string read getRaisonSociale
+      write setRaisonSociale;
+    class property IDTypeEtablissement: integer read getIDTypeEtablissement
+      write setIDTypeEtablissement;
+  end;
+
+implementation
+
+uses uParam;
+
+const
+  CKEYID = 'EtbID';
+  CKEYRaisonSociale = 'EtbRaisonSociale';
+  CKEYIDTypeEtablissement = 'EtbIDTypeEtablissement';
+
+  { TConfig }
+
+class function TConfig.getID: integer;
+begin
+  result := tParams.getValue(CKEYID, -1);
+end;
+
+class function TConfig.getIDTypeEtablissement: integer;
+begin
+  result := tParams.getValue(CKEYIDTypeEtablissement, -1);
+end;
+
+class function TConfig.getRaisonSociale: string;
+begin
+  result := tParams.getValue(CKEYRaisonSociale, '');
+end;
+
+class procedure TConfig.setID(const Value: integer);
+begin
+  tParams.setValue(CKEYID, Value);
+end;
+
+class procedure TConfig.setIDTypeEtablissement(const Value: integer);
+begin
+  tParams.setValue(CKEYIDTypeEtablissement, Value);
+end;
+
+class procedure TConfig.setRaisonSociale(const Value: string);
+begin
+  tParams.setValue(CKEYRaisonSociale, Value);
+end;
+
+end.
--- README.md
+++ README.md
@@ -5,3 +5,6 @@ TODO :
 - ajouter les index qui conviennent à la base de données du serveur
 - sécuriser les appels API et la base de données pour éviter les bidouilles
 - faire la version PHP du serveur (pour le site https://cctrb.fr )
+
+Dependance :
+https://github.com/DeveloppeurPascal/librairies stocké dans ../librairies
