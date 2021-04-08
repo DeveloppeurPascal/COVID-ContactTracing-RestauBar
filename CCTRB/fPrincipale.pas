4490139da6bbc19c850bc82a8d5a7e1f4b3f4bdb
patrick.premartin@developpeur-pascal.fr <Patrick>
2021-04-08 16:16:50 +0200

    menu opérationnel sur application cliente

--- CCTRB/fPrincipale.pas
+++ CCTRB/fPrincipale.pas
@@ -3,11 +3,24 @@ unit fPrincipale;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  System.SysUtils, System.Types, System.UITypes, System.Classes,
+  System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Layouts,
+  FMX.Objects, FMX.Effects;
 
 type
   TfrmPrincipale = class(TForm)
+    VertScrollBox1: TVertScrollBox;
+    FlowLayout1: TFlowLayout;
+    btnEntrer: TButton;
+    StyleBook1: TStyleBook;
+    btnSortir: TButton;
+    btnDeclarerPositivite: TButton;
+    btnTestCasContact: TButton;
+    btnInfosSurLeLogiciel: TButton;
+    procedure btnEntrerClick(Sender: TObject);
+    procedure FormResize(Sender: TObject);
   private
     { Déclarations privées }
   public
@@ -20,5 +33,25 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TfrmPrincipale.btnEntrerClick(Sender: TObject);
+begin
+  showmessage('test');
+end;
+
+procedure TfrmPrincipale.FormResize(Sender: TObject);
+var
+  i: integer;
+  c: tcontrol;
+begin
+  FlowLayout1.height := 100;
+  for i := 0 to FlowLayout1.ChildrenCount - 1 do
+    if FlowLayout1.Children[i] is tcontrol then
+    begin
+      c := FlowLayout1.Children[i] as tcontrol;
+      if FlowLayout1.height < c.height + c.Position.y then
+        FlowLayout1.height := c.height + c.Position.y + 10;
+    end;
+end;
 
 end.
