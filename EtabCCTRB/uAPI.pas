7136f5aff61099e84fd7d9e804d1c0f7457123ec
patrick.premartin@developpeur-pascal.fr <Patrick>
2021-04-30 16:53:03 +0200

    ajout des configurations WAMP - WebBrocker sur EtabCCTRB et CCTRB

--- EtabCCTRB/uAPI.pas
+++ EtabCCTRB/uAPI.pas
@@ -12,14 +12,23 @@ implementation
 function getAPIURL: string;
 begin
 {$IFDEF DEBUG}
+{$IF Defined(CCTRB_WEBBROCKER)}
 {$IFDEF MSWINDOWS}
   result := 'http://localhost:8080/';
 {$ELSE}
   result := 'http://192.168.1.169:8080/';
 {$ENDIF}
+{$ELSEIF Defined(CCTRB_WAMP)}
+{$IFDEF MSWINDOWS}
+  result := 'http://localhost/cctrb/';
+{$ELSE}
+  result := 'http://192.168.1.169/cctrb/';
+{$ENDIF}
+{$ELSE}
+{$MESSAGE FATAL 'Choisir un serveur d''API.'}
+{$ENDIF}
 {$ELSE}
   result := 'https://api.cctrb.fr/';
-  // TODO : gérer éventuellement la version de l'API
 {$ENDIF}
 end;
 
