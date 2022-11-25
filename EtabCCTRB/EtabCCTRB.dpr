0887a0e223152300590d263e2676cceb8467dcc2
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2022-11-25 20:11:48 +0100

    updated references to submodule units

--- EtabCCTRB/EtabCCTRB.dpr
+++ EtabCCTRB/EtabCCTRB.dpr
@@ -8,16 +8,16 @@ uses
   uAPI_typesetb in 'uAPI_typesetb.pas',
   uAPI in 'uAPI.pas',
   uDM in 'uDM.pas' {dm: TDataModule},
-  uParam in '..\..\Librairies\uParam.pas',
   uConfig in 'uConfig.pas',
   fVisualiser in 'fVisualiser.pas' {frmVisualiser},
   fModifier in 'fModifier.pas' {frmModifier},
   fTestCasContact in 'fTestCasContact.pas' {frmTestCasContact},
   fGenerationQRCode in 'fGenerationQRCode.pas' {frmGenerationQRCode},
-  DelphiZXIngQRCode in '..\..\..\foxitsoftware\DelphiZXingQRCode\Source\DelphiZXIngQRCode.pas',
-  uChecksumVerif in '..\..\Librairies\uChecksumVerif.pas',
   uCCTRBPrivateKey in '..\uCCTRBPrivateKey.pas',
-  u_md5 in '..\..\Librairies\u_md5.pas';
+  Olf.RTL.Params in '..\lib-externes\librairies\Olf.RTL.Params.pas',
+  uChecksumVerif in '..\lib-externes\librairies\uChecksumVerif.pas',
+  u_md5 in '..\lib-externes\librairies\u_md5.pas',
+  DelphiZXIngQRCode in '..\lib-externes\DelphiZXingQRCode\Source\DelphiZXIngQRCode.pas';
 
 {$R *.res}
 
