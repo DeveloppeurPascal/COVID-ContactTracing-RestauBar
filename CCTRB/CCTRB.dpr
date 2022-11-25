c9a060ba2de331d57cb1a96a26c8694ef36a5d93
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2022-11-25 20:04:47 +0100

    updated external units dependencies

--- CCTRB/CCTRB.dpr
+++ CCTRB/CCTRB.dpr
@@ -5,19 +5,16 @@ uses
   FMX.Forms,
   fPrincipale in 'fPrincipale.pas' {frmPrincipale},
   uAPI in '..\EtabCCTRB\uAPI.pas',
-  uParam in '..\..\Librairies\uParam.pas',
   uConfig in 'uConfig.pas',
   UAPI_cli in 'UAPI_cli.pas',
   fLectureQRcode in 'fLectureQRcode.pas' {frmLectureQRcode},
   fAPropos in 'fAPropos.pas' {frmAPropos},
   fTestCasContact in 'fTestCasContact.pas' {frmTestCasContact},
-  ZXing.ScanManager in '..\..\..\Spelt\ZXing.Delphi\Lib\Classes\ZXing.ScanManager.pas',
-  ZXing.BarcodeFormat in '..\..\..\Spelt\ZXing.Delphi\Lib\Classes\Common\ZXing.BarcodeFormat.pas',
-  ZXing.ReadResult in '..\..\..\Spelt\ZXing.Delphi\Lib\Classes\Common\ZXing.ReadResult.pas',
   uRoutines in 'uRoutines.pas',
-  uChecksumVerif in '..\..\Librairies\uChecksumVerif.pas',
   uCCTRBPrivateKey in '..\uCCTRBPrivateKey.pas',
-  u_md5 in '..\..\Librairies\u_md5.pas';
+  Olf.RTL.Params in '..\lib-externes\librairies\Olf.RTL.Params.pas',
+  uChecksumVerif in '..\lib-externes\librairies\uChecksumVerif.pas',
+  u_md5 in '..\lib-externes\librairies\u_md5.pas';
 
 {$R *.res}
 
